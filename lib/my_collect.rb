a73afd9b7160a8cce6b4690af5402862ee1c4f3b
Kathy Nguyen <kathyvng@gmail.com>
2018-06-27 04:33:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,8 @@
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+
+def my_collect
+  empty_array = []
+  counter = 0
+    my_collect(empty_array) do |x|
+      counter += 1
+    end
 end
